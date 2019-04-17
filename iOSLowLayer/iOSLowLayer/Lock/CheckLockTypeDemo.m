85f37f5517f20a2491ff81efd3611d60730d5fda
tang <itangzhentao@126.com>
2019-04-17 19:44:02 +0800

    m

--- iOSLowLayer/iOSLowLayer/Lock/CheckLockTypeDemo.m
+++ iOSLowLayer/iOSLowLayer/Lock/CheckLockTypeDemo.m
@@ -9,11 +9,13 @@
 #import "CheckLockTypeDemo.h"
 #import <libkern/OSAtomic.h>
 #import <pthread.h>
+#import <os/lock.h>
 
 @interface CheckLockTypeDemo ()
 
 @property (assign, nonatomic) OSSpinLock saleTicketLock; // 自旋锁
 @property (assign, nonatomic) pthread_mutex_t mutexLock; // 互斥锁
+@property (assign, nonatomic) os_unfair_lock unfairLock; // 不公锁
 
 @end
 
@@ -32,6 +34,9 @@
         pthread_mutexattr_init(&attr);
         pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_DEFAULT);
         pthread_mutex_init(&_mutexLock, &attr);
+        
+        // 初始化不公锁
+        _unfairLock = OS_UNFAIR_LOCK_INIT;
 
     }
     return self;
@@ -46,14 +51,17 @@
 {
     NSLog(@"[%@ %@]: out of lock.", [self class], NSStringFromSelector(_cmd));
 //    OSSpinLockLock(&_saleTicketLock);
-    pthread_mutex_lock(&_mutexLock);
+//    pthread_mutex_lock(&_mutexLock);
+    os_unfair_lock_lock(&_unfairLock);
     NSLog(@"[%@ %@]: enter into lock.", [self class], NSStringFromSelector(_cmd));
     
     [super saleTicket];
     
     sleep(1000);
 //    OSSpinLockUnlock(&_saleTicketLock);
-    pthread_mutex_unlock(&_mutexLock);
+//    pthread_mutex_unlock(&_mutexLock);
+    os_unfair_lock_unlock(&_unfairLock);
+
 
     NSLog(@"[%@ %@]: finish", [self class], NSStringFromSelector(_cmd));
 

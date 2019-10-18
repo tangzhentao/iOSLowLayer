5a92a313990105beb84c95c3fbeebbb7e82f35dd
tang <itangzhentao@126.com>
2019-10-18 17:59:11 +0800

    add isa

---
+++ isa_superclass/isa_superclass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  isa_superclass
+//
+//  Created by tang on 2019/10/18.
+//  Copyright © 2019 genghaowan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

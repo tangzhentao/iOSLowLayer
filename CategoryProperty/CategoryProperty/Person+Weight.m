62246f95567595f6441b605619f98e832919ebe6
tang <itangzhentao@126.com>
2019-11-04 11:33:11 +0800

    tmp

--- CategoryProperty/CategoryProperty/Person+Weight.m
+++ CategoryProperty/CategoryProperty/Person+Weight.m
@@ -7,10 +7,51 @@
 //
 
 #import "Person+Weight.h"
-
-#import <AppKit/AppKit.h>
-
+#import "objc/runtime.h"
 
 @implementation Person (Weight)
+
+#pragma mark - 1、static const void *weight_key -- NULL
+
+/*
+static const void *weightKey; // 没有赋值，值为NULL
+
+- (void)setWeight:(int)weight
+{
+    objc_setAssociatedObject(self, weightKey, @(weight), OBJC_ASSOCIATION_ASSIGN);
+}
+
+- (int)weight
+{
+    return [objc_getAssociatedObject(self, weightKey) intValue];
+}
+*/
+
+#pragma mark - 2、指向自己的指针
+
+/*
+ static const void *weightKey = &weightKey;
+ 
+ - (void)setWeight:(int)weight
+ {
+     objc_setAssociatedObject(self, weightKey, @(weight), OBJC_ASSOCIATION_ASSIGN);
+ }
+ 
+ - (int)weight
+ {
+     return [objc_getAssociatedObject(self, weightKey) intValue];
+ }
+*/
+#pragma mark - 2、指向自己的指针
+
+static char weightKey;
+
+- (void)setWeight:(int)weight {
+    objc_setAssociatedObject(self, &weightKey, @(weight), OBJC_ASSOCIATION_ASSIGN);
+}
+
+- (int)weight {
+    return [objc_getAssociatedObject(self, &weightKey) intValue];
+}
 
 @end
--- CategoryProperty/CategoryProperty/main.m
+++ CategoryProperty/CategoryProperty/main.m
@@ -7,11 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Person+Weight.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        Person *p1 = [Person new];
+        p1.weight = 10;
+        
+        NSLog(@"p1 weight: %d", p1.weight);
+        
+        Person *p2 = [Person new];
+        p2.weight = 20;
+        
+        NSLog(@"p2 weight: %d", p2.weight);
     }
     return 0;
 }
